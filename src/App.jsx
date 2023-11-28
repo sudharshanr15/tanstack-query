6b83e67126c9ceeb09089186d4e8814e88807e81
Sudharshan <sudharshanrms2004@gmail.com>
2023-11-28 23:32:48 +0530

    Basic tanstack usages

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import {useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
+
+const POSTS = [
+  {
+    id: 1,
+    title: "Post 1"
+  },
+  {
+    id: 2,
+    title: "Post 2"
+  }
+]
+
+function App(){
+  const queryClient = useQueryClient()
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => POSTS.push({id: crypto.randomUUID(), title}))
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    }
+  })
+
+  if(postsQuery.isLoading) return <h1>Loading...</h1>
+  if(postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>
+
+  console.log(newPostMutation.isLoading)
+
+  return <div>
+    {postsQuery.data.map(post => (
+      <div key={post.id}>
+        {post.title}
+      </div>
+    ))}
+    <button disabled={newPostMutation.isLoading} onClick={() => newPostMutation.mutate("New post")}>
+      Add New
+    </button>
+  </div>
+}
+
+
+function wait(duration){
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
+
+export default App
